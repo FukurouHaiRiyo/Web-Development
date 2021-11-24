5dae8d882d64dc7ef79dcf269f6b3ab97f33c87e
comby <53621037+comby-cmyk@users.noreply.github.com>
2021-11-24 19:41:07 +0200

    Create index.php

---
+++ 25-Oct/index.php
@@ -0,0 +1,93 @@
+<?php
+// print("hello");
+// $name_var = array(1, 5, 6);
+// echo strlen("hey");
+// echo $name_var[1];
+
+// echo strrepl("Hello World", "World", "Hey");
+
+// // echo PHP_INT_MAX;
+// $var = 4;
+// echo is_int($var);
+
+// echo PHP_FLOAT_MAX;
+
+// echo is_float(3.4); //echo is_double(3.4);
+// $v = 1.251;
+// echo (int)$v;
+// $v1 = 2;
+// echo (float)$v1;
+
+// echo pi();
+// echo min(1, 7, 3, 25);
+// echo "\n";
+// echo max(1, 3, 25);
+
+// echo abs(-2);
+// echo sqrt(4);
+// echo round(0.6);
+// echo rand();
+// echo "\n";
+// echo rand(10, 100);
+
+// define(TEST_CONST, 10);
+// echo TEST_CONST;
+
+// operatori +, -, /, %, **(raise to power), *;
+// asignation =, ==, +=, -=, /=, %=;
+// comparation ==, ===, !=, !==, <, >, <=, >=
+
+// increment/decrement
+// ++$v, --$v, $v--, $v++
+
+// logical operators &&(and), ||(or), !=(not)
+
+// string .(concat) -> $v1.$v2
+          // .=asignation -> $v1.=$v2
+          
+/*
+    vectors: $v1 + $v2 concat
+    ==, !=, ===, !==
+
+    if(condition){
+
+    }else if(condition){
+
+    }else{
+
+    }
+
+    while(condition){
+
+    }
+
+    do{
+
+    }while(condition)
+
+    for(int; condition; step){
+
+    }
+
+    foreach($vect as $val){
+
+    }
+
+    foreach($vect as $i => #val){
+
+    }
+
+    ["car" => 23,]
+    array("car" => 23, ...)
+
+
+    count($v);
+    sort($v);
+    rsort($v);
+
+    ksort($v) puts indexes in order;
+    krsort($v) indexes in descending order;
+    asort($v);
+    arsort($v);
+*/
+?>
