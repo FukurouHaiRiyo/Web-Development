a457dc04a6f4e52bfef63ffbfb156041e34ee0e0
comby <53621037+comby-cmyk@users.noreply.github.com>
2021-12-13 20:32:40 +0200

    Create index.php

---
+++ 13Dec/index.php
@@ -0,0 +1,12 @@
+<!-- make form that send user to welcome.php -->
+<!-- <form action="welcome.php" method="get">
+    <label for="user">Username:</label>
+    <input type="text" name="user" id="user">
+    <input type="submit" value="Submit">
+</form> -->
+
+<form action="welcome.php" method="post">
+    <label for="user">Username:</label>
+    <input type="text" name="user" id="user">
+    <input type="submit" value="Submit">
+</form>
